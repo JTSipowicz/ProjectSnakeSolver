fb0e908e22c6e146d2358648937b3f049ed3be2e
James Sipowicz III <55172879+JTSipowicz@users.noreply.github.com>
2021-03-31 01:54:49 -0500

    Add files via upload
    Attempted to fix the snake not appearing. Fix and commit as a branch!

--- snakeSolver.py
+++ snakeSolver.py
@@ -9,49 +9,50 @@ white = (255,255,255)
 green = (0,255,0)
 black = (0, 0, 0)
 
-dis_width = 500
-dis_height  = 500
-dis = pygame.display.set_mode((dis_width, dis_width))
+displayWidth = 500
+displayHeight  = 500
+dis = pygame.display.set_mode((displayWidth, displayWidth))
 pygame.display.set_caption('JSIPs Snake')
 
 clock = pygame.time.Clock()
-snake_block = 10
-snake_speed = 25
+snakeBody = 10
+snakeSpeed = 25
 
 font_style = pygame.font.SysFont(None, 25)
 score_font = pygame.font.SysFont(None, 25)
 
+def ourSnake(snakeBody, snakeList):
+	for x in snakeList:
+		pygame.draw.rect(dis, green, [x[0], x[1], snakeBody, snakeBody])
+
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/6, dis_height/3])
+    dis.blit(mesg, [displayWidth/6, displayHeight/3])
 
 def displayedScore(currentScore):
 	viewableScore = score_font.render("Current Score: " + str(currentScore), True, green)
 	dis.blit(viewableScore, [0,0])
 
-def ourSnake(snake_block, snakeList):
-	for x in snakeList:
-		pygame.draw.rect(dis, white, [x[0], x[1], snake_block, snake_block])
-
 def gameLoop():
 	gameOver = False
 	gameClose = False
 
-	x1 = dis_width/2
-	y1 = dis_height/2
+	x1 = displayWidth/2
+	y1 = displayHeight/2
 	x1_change = 0       
 	y1_change = 0
 	snakeList = []
-	Length_of_snake = 1
+	snakeLength = 2
 
-	foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-	foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+	foodx = round(random.randrange(0, displayWidth - snakeBody) / 10.0) * 10.0
+	foody = round(random.randrange(0, displayHeight - snakeBody) / 10.0) * 10.0
 	
 	while not gameOver:
 		while gameClose == True:
 			dis.fill(white)
 			message ("Game Over! Press Q to quit or C to try again...", red)
-			displayedScore(Length_of_snake - 1)
+			displayedScore(snakeLength - 1)
+
 			pygame.display.update()
 
 			for event in pygame.event.get():
@@ -61,49 +62,51 @@ def gameLoop():
 						gameClose = False
 					if event.key == pygame.K_c:
 						gameLoop()
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				gameOver = True
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
-					x1_change = -snake_block
+					x1_change = -snakeBody
 					y1_change = 0
 				elif event.key == pygame.K_RIGHT:
-					x1_change = snake_block
+					x1_change = snakeBody
 					y1_change = 0
 				elif event.key == pygame.K_UP:
-					y1_change = -snake_block
+					y1_change = -snakeBody
 					x1_change = 0
 				elif event.key == pygame.K_DOWN:
-					y1_change = snake_block
+					y1_change = snakeBody
 					x1_change = 0
-		if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+		if x1 >= displayWidth or x1 < 0 or y1 >= displayHeight or y1 < 0:
 			gameClose = True
 		x1 += x1_change
 		y1 += y1_change
-		pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
+		dis.fill(black)
+		pygame.draw.rect(dis, blue, [foodx, foody, snakeBody, snakeBody])
 		snakeHead = []
 		snakeHead.append(x1)
 		snakeHead.append(y1)
 		snakeHead.append(snakeHead)
-		if len(snakeList) > Length_of_snake:
+		if len(snakeList) > snakeLength:
 			del snakeList[0]
 
 		for x in snakeList[:-1]:
 			if x == snakeHead:
 				gameClose = True
 
-		ourSnake(snake_block, snakeList)
-		displayedScore(Length_of_snake - 1)
+		ourSnake(snakeBody, snakeList)
+		displayedScore(snakeLength - 1)
 
 		pygame.display.update()
 
 		if x1 == foodx and y1 == foody:
-			foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-			foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-			Length_of_snake += 1
+			foodx = round(random.randrange(0, displayWidth - snakeBody) / 10.0) * 10.0
+			foody = round(random.randrange(0, displayWidth - snakeBody) / 10.0) * 10.0
+			snakeLength += 1
 
-		clock.tick(snake_speed)
+		clock.tick(snakeSpeed)
 
 	pygame.quit()
 	quit()
